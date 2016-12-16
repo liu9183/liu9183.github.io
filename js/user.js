c2dfb1c0846eb59e683889769c99a8d65c4ffbab
liu9183 <710930665@qq.com>
2016-12-16 10:40:20 +0800

    麻雀粮仓

--- js/user.js
+++ js/user.js
@@ -1,12 +1,13 @@
-$(document).ready(function(){
-	 judgeorder();
-	 $(".order_action_cancel").on("click",function(){
-            $(this).parents(".order_form").remove();
-            judgeorder();
-        })
+$(document).ready(function() {
+	judgeorder();
+	$(".order_action_cancel").on("click", function() {
+		$(this).parents(".order_form").remove();
+		judgeorder();
+	})
 })
-function judgeorder(){
-	 if($(".order_form").length<1){
-                $(".null_order").show();
-            }
+
+function judgeorder() {
+	if($(".order_form").length < 1) {
+		$(".null_order").show();
+	}
 }
